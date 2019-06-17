c9a5a91ae270ecfacef71bda27e32a051a32bb4b
OlegKorn <kornilovoy@gmail.com>
2019-06-17 17:44:42 +0300

    first commit

--- config/db.php
+++ config/db.php
@@ -1,22 +1,19 @@
 <?php
 
-  $dblocation = "localhost";
-  $dbname = "LOCAL.SHOP";
-  $dbuser = "root";
-  $dbpasswd = " ";
-
-  global $db;
-  //connect to DB
-  $db = new mysqli($dblocation, $dbuser, $dbpasswd, $dbname);
-
-  mysqli_set_charset($db, 'utf8');
-
-  if($db->connect_errno) {
-  	die("Ошибка доступа к MySQL");
-  }
+  function db() {
+    
+    $dblocation = "localhost";
+    $dbname = "LOCAL.SHOP";
+    $dbuser = "root";
+    $dbpasswd = "oleg1811";
   
-  if(!mysqli_select_db($db, $dbname)) {
-  	echo "Ошибка доступа к базе данных: ($dbname)";
-  	exit();
+    $db = new mysqli($dblocation, $dbuser, $dbpasswd, $dbname);
+
+    mysqli_set_charset($db, 'utf8');
+
+    if($db->connect_errno) {
+      die("Ошибка доступа к MySQL");
+    }
+
   }
 
--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -1,11 +1,23 @@
 <?php
 
-
-  
-
-
   function getAllMainCatsWithChildren() {    
 
-    echo 'test';
-  	     
+  	//to fix a bug  Warning: mysqli_query() expects at least 2 parameters, 1 given in /opt/lampp/htdocs/site.local/models/CategoriesModel.php on line 15
+  	//insert lines 7:12 + gave $bd param to line 16
+
+  	$dblocation = "localhost";
+    $dbname = "LOCAL.SHOP";
+    $dbuser = "root";
+    $dbpasswd = "oleg1811";
+  
+    $db = new mysqli($dblocation, $dbuser, $dbpasswd, $dbname);
+
+    $sql = 'SELECT * FROM categories WHERE parent_id=0';
+
+    $rs = mysqli_query($db, $sql);
+
+    while($row = mysqli_fetch_assoc($rs)) {
+      echo 'id = ' . $row['id'] . '<br/>';
+    } 
+
   }
